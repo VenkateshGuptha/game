645d67c7eaa2ca75e5b486371313f5741e12ae46
VenkateshGuptha <venkateshguptha@gmail.com>
2021-10-29 21:08:27 +0530

    AgGrid dataframe

--- apps/leaderboard_app.py
+++ apps/leaderboard_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from hydralit import HydraHeadApp
 from pymongo import MongoClient
 import numpy as np
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 
 class LeaderBoardApp(HydraHeadApp):
      def __init__(self, title = 'Hydralit Explorer', **kwargs):
@@ -10,14 +13,20 @@ class LeaderBoardApp(HydraHeadApp):
         self.UserNames = {}
 
      def run(self):
-        st.text("LeaderBoard")
         self.LoadData()
 
         data = []
         for row in self.currentPuzzleData:
             data.append((self.GetUserName(str(row['userid'])), row['infilldistance'], row['totaldistance']))
         
-        st.dataframe(np.array(data))
+        data = pd.DataFrame(np.array(data), columns=['Username', 'InFile distance', 'Total Distance'])
+        gb = GridOptionsBuilder.from_dataframe(data)
+        gb.configure_selection(selection_mode='single')
+        go = gb.build()
+        #st.dataframe(data)
+        data = AgGrid(data, gridOptions=go, update_mode=GridUpdateMode.SELECTION_CHANGED)
+        selecteddf = pd.DataFrame(data['selected_rows'])
+        st.text(selecteddf)
 
      def LoadData(self):
         client = MongoClient(st.secrets["url"])
@@ -30,14 +39,9 @@ class LeaderBoardApp(HydraHeadApp):
         client = MongoClient(st.secrets["url"])
         db = client.Game
         return db.Users.find()
-        '''user = db.Users.find_one({'_id':userid})
-        if user is None:
-            return ""
 
-        return user['username']'''
-
+     #@st.cache(persist=True, hash_funcs={st.delta_generator.DeltaGenerator: id, pymongo.write_concern.WriteConcern: id})
      def GetUserName(self, userid):
-
         for user in self.Users:
             if str(user['_id']) == userid:
                 return user['username']
